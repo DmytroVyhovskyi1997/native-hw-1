a8d4331559fe2dca9a1add4884ba3587588eada7
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-06-25 12:09:16 +0300

    add location

--- Screens/main/CreatePostsScreen.js
+++ Screens/main/CreatePostsScreen.js
@@ -1,14 +1,28 @@
 import { View, Text, StyleSheet, Image, TextInput } from "react-native";
 import { Ionicons } from "@expo/vector-icons"; 
 import { Camera } from "expo-camera";
+import * as Location from "expo-location";
 import {  TouchableOpacity } from "react-native-gesture-handler";
 import { useState, useEffect } from "react";
 
 export const CreatePostsScreen = ({navigation}) => {
   const [camera, setCamera] = useState(null);
   const [photo, setPhoto] = useState(null);
+   const [location, setLocation] = useState(null);
+   const [errorMsg, setErrorMsg] = useState(null);
 
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
 
+      const location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
 
 
   const takePhoto = async () => {
@@ -113,7 +127,7 @@ const styles = StyleSheet.create({
     color: "#BDBDBD",
   },
   inputTitle: {
-    color: "#BDBDBD",
+   
     marginLeft: 16,
     marginTop: 29,
     paddingBottom: 15,
